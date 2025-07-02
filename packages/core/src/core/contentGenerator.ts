4120038fa8ff44512edade5b102d7ea8fd7c27fd
warjiang <1096409085@qq.com>
2025-07-02 15:43:13 +0800

    feat(core): apply proxy in core & telemetry pkg

--- packages/core/src/core/contentGenerator.ts
+++ packages/core/src/core/contentGenerator.ts
@@ -86,6 +86,7 @@ export async function createContentGeneratorConfig(
     contentGeneratorConfig.model = await getEffectiveModel(
       contentGeneratorConfig.apiKey,
       contentGeneratorConfig.model,
+      contentGeneratorConfig.proxy,
     );
 
     return contentGeneratorConfig;
